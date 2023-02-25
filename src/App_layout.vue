3b8820b5b2b8f3afdf14ce00b4e8a0b0a13300cc
PDH-NOTEBOOK <ehdgnl4656@naver.com>
2023-02-25 13:37:03 +0900

    third commit

---
+++ src/App_layout.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-app>
+    <v-app-bar app> </v-app-bar>
+    <v-navigation-drawer app> </v-navigation-drawer>
+    <v-main>
+      <v-container fluid> 컨텐츠 영역 </v-container>
+    </v-main>
+    <v-footer> footer </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
